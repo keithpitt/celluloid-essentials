a301b9fdcd6f67a3c1af3929181ff46583c02ed9
//de <de@freed.network>
2015-04-25 15:10:54 -0700

    mid-way through changes to registry to suite celluloid-supervisor

--- lib/celluloid/internals/registry.rb
+++ lib/celluloid/internals/registry.rb
@@ -5,53 +5,101 @@ module Celluloid
     # The Registry allows us to refer to specific actors by human-meaningful names
     class Registry
       def initialize
-        @registry = {}
-        @registry_lock = Mutex.new
+        @root = nil     # keep root out of the standard list of registered names
+        @actors = {}    # hash of name => actor
+        @index = {}     # hash of name => branch
+        @branches = {}  # hash of branch => [ actors ]
+        @registry = Mutex.new
       end
 
       # Register an Actor
       def []=(name, actor)
-        actor_singleton = class << actor; self; end
-        unless actor_singleton.ancestors.include? Proxy::Abstract
-          raise TypeError, "not an actor"
-        end
+        #de REMOVE puts "ACTORS: #{@actors.keys}"
+        #de REMOVE puts "BRANCHES: #{@branches.keys}"
+        if name == :root
+          @registry.synchronize {
+            @root = actor
+          }
+        else
+          actor_singleton = class << actor; self; end
+          unless actor_singleton.ancestors.include? Proxy::Abstract
+            raise TypeError, "not an actor"
+          end
 
-        @registry_lock.synchronize do
-          @registry[name.to_sym] = actor
+=begin
+          if actor.class.ancestors.include? Supervision::Container
+            puts "Supervisor: #{actor.links.inspect}"
+          end
+=end
+          @registry.synchronize do
+            @actors[name.to_sym] = actor
+          end
+          actor.mailbox << NamingRequest.new(name.to_sym)
         end
+      end
 
-        actor.mailbox << NamingRequest.new(name.to_sym)
+      def add(name,actor,branch=:services)
+        set(name, actor)
+        @registry.synchronize {
+          unless @branches.key? branch
+            @branches[branch] = []
+            #de REMOVE puts "MAKING BRANCH ON REGISTRY: #{branch}"
+            self.class.instance_eval {
+              remove_method(branch) rescue nil
+              define_method(branch) { @branches[branch] }
+            }
+            @branches[branch] << name
+          end
+          @index[name.to_sym] = branch
+        }
       end
 
       # Retrieve an actor by name
       def [](name)
-        @registry_lock.synchronize do
-          @registry[name.to_sym]
-        end
+        return @root if name == :root
+        @registry.synchronize {
+          @actors[name.to_sym]
+        }
+      end
+
+      def branch(name)
+        @registry.synchronize {
+          @index.select { |a,b| b == name }
+        }
       end
 
       alias_method :get, :[]
       alias_method :set, :[]=
 
       def delete(name)
-        @registry_lock.synchronize do
-          @registry.delete name.to_sym
-        end
+        @registry.synchronize {
+          @actors.delete name.to_sym
+          @index.delete name.to_sym
+        }
+      end
+
+      def include? name
+        names.include? name
       end
 
       # List all registered actors by name
       def names
-        @registry_lock.synchronize { @registry.keys }
+        @registry.synchronize { @actors.keys }
+      end
+
+      def index
+        @registry.synchronize { @index }
       end
 
       # removes and returns all registered actors as a hash of `name => actor`
       # can be used in testing to clear the registry
       def clear
         hash = nil
-        @registry_lock.synchronize do
-          hash = @registry.dup
-          @registry.clear
-        end
+        @registry.synchronize {
+          hash = @actors.dup
+          @actors.clear
+          @index.clear
+        }
         hash
       end
     end
